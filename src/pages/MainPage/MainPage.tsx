3c8964adb9ff9f7c51748373555aff9fa6a89a61
Siko <dudashvilivladislav@gmail.com>
2025-10-07 13:31:33 +0300

    fix: add dockerfile

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import { useTranslation } from 'react-i18next';
 
+import cls from './MainPage.module.scss';
+
 export const MainPage: React.FC = () => {
   const { t } = useTranslation();
 
   return (
     <div>
       {t('Главная страница')}
+      <h1 className={cls.title}>NEWWWWWWW</h1>
     </div>
   );
 };
